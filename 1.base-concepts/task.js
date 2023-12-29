6186e54c77d6a346046d0a65f7a8f67864c21b74
Maksim Chalyh <nazgulius@yandex.ru>
2023-12-29 23:09:08 +0300

    8 test done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,7 +5,7 @@ function solveEquation(a, b, c) {
   let arr = [];
   let d = Math.pow(b, 2) - 4 * a * c;  
   if (d < 0) {
-    arr.push(0);
+    arr.push();
   } if (d === 0) {
     arr.push(-b / (2 * a));
   } if (d > 0) {
@@ -17,13 +17,10 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percentPlus = percent / 100;
-  let betMounth = percentPlus / 12;
-  let months = 12;
-  let sumCredit = amount - contribution;
-  let partPercent = percent / 12;
-  let paymentMonths = amount * (partPercent + (partPercent / (Math.pow((1 + partPercent), countMonths) - 1)));
-  let sumAll = (paymentMonths * countMonths) +  contribution;
+  let percentPlus = (percent / 100) / 12;   
+  let sumCredit = amount - contribution;  
+  let paymentMonths = sumCredit * (percentPlus + (percentPlus / (Math.pow((1 + percentPlus), countMonths) - 1)));
+  let sumAll = paymentMonths * countMonths;
   let sum = (sumAll).toFixed(2);
 
   return Number(sum);
