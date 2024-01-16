6760d32192dc858ae0c3c8ba539b796657e5050d
Maksim Chalyh <nazgulius@yandex.ru>
2024-01-16 18:04:17 +0300

    Задание 3 начато

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+  this.name = name, 
+  this.gender = gender, 
+  this.age = age, 
+  this.marks = []  
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marks) { 
+  if (this.marks.length === 0){
+    
+  }
+
+  this.marks = [...marks];
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks.length === 0){
+    return 0;
+  }
+
+  let sum = this.marks.reduce((elementOne, elementTwo) => elementOne + elementTwo);
+  let avg = Number((sum / this.marks.length).toFixed(0));
+
+  return avg;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
+
+let student1 = new Student("Вася", "мужской", 22);
+student1.setSubject("Algebra");
+student1.addMarks(4, 5, 4, 5);
+console.log(student1);
