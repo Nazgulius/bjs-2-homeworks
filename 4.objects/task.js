dc5bcf1e69ca969c63887abec73ad4b45c638118
Maksim Chalyh <nazgulius@yandex.ru>
2024-01-17 00:01:55 +0300

    5 из 6 тестов. Cannot read properties of undefined (reading 'push')

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,8 +1,8 @@
 function Student(name, gender, age) {
-  this.name = name, 
-  this.gender = gender, 
-  this.age = age, 
-  this.marks = []  
+    this.name = name, 
+    this.gender = gender, 
+    this.age = age, 
+    this.marks = []  
 }
 
 Student.prototype.setSubject = function (subjectName) {
@@ -10,31 +10,25 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) { 
-  if (this.marks.length === 0){
-    
-  }
+    if (Array.isArray(this[marks])) {
+        return 0;
+    }
 
-  this.marks = [...marks];
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  if (this.marks.length === 0){
-    return 0;
-  }
+    if (this.marks.length === 0){
+      return 0;
+    }
 
-  let sum = this.marks.reduce((elementOne, elementTwo) => elementOne + elementTwo);
-  let avg = Number((sum / this.marks.length).toFixed(0));
+    let sum = this.marks.reduce((elementOne, elementTwo) => elementOne + elementTwo);
 
-  return avg;
+    return Number((sum / this.marks.length).toFixed(0));
 }
 
 Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
-
-let student1 = new Student("Вася", "мужской", 22);
-student1.setSubject("Algebra");
-student1.addMarks(4, 5, 4, 5);
-console.log(student1);
