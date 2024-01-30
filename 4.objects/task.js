e6a4719cff4e7c08e193fe7ca9e0d5c03271c36f
Maksim Chalyh <nazgulius@yandex.ru>
2024-01-30 23:55:34 +0300

    Task 6 start. 4/15 test

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,8 +1,10 @@
-function Student(name, gender, age) {
-    this.name = name, 
-    this.gender = gender, 
-    this.age = age, 
-    this.marks = []  
+class Student {
+    constructor (name, gender, age) {
+      this.name = name, 
+      this.gender = gender, 
+      this.age = age, 
+      this.marks = []  
+    }
 }
 
 Student.prototype.setSubject = function (subjectName) {
--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,21 @@
-﻿
+﻿function parseCount (value) {
+    let num = Number.parseFloat(value);       
+    if (isNaN(num)) {
+        throw new Error("Невалидное значение");        
+    } else {
+        return num;
+    } 
+}
+
+function validateCount (value) {
+    try {
+        let num = parseCount(value);
+        if (num) {
+            return num;
+        }
+               
+    } catch (error) {
+        console.log(error);
+    } 
+}
+
